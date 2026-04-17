d3d989b6ea2e5b3e29d6fc745005cbe884199bfe
CrqzyCat <powerjak71@gmail.com>
2026-04-17 21:22:49 +0200

    Refactor AdvancementToastMixin to trigger video playback on toast addition

--- src/client/java/me/thukunua/thukuna/client/mixin/AdvancementToastMixin.java
+++ src/client/java/me/thukunua/thukuna/client/mixin/AdvancementToastMixin.java
@@ -1,20 +1,22 @@
 package me.thukunua.thukuna.client.mixin;
 
 import me.thukunua.thukuna.client.ThukunaClient;
-import net.minecraft.advancement.AdvancementEntry;
 import net.minecraft.client.toast.AdvancementToast;
+import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AdvancementToast.class)
+@Mixin(ToastManager.class)
 public class AdvancementToastMixin {
 
-    @Inject(method = "show", at = @At("HEAD"))
-    private void onAdvancementToastShow(ToastManager manager, CallbackInfo ci) {
-        ThukunaClient.LOGGER.info("Thukuna: Advancement bekommen! Video wird abgespielt...");
-        ThukunaClient.shouldShowVideo = true;
+    @Inject(method = "add", at = @At("HEAD"))
+    private void onToastAdded(Toast toast, CallbackInfo ci) {
+        if (toast instanceof AdvancementToast) {
+            ThukunaClient.LOGGER.info("Thukuna: Advancement bekommen! Video wird abgespielt...");
+            ThukunaClient.shouldShowVideo = true;
+        }
     }
 }
