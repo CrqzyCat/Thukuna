60cd8dfd559f6c29bfe8ce078c539074a035ae7d
CrqzyCat <powerjak71@gmail.com>
2026-04-17 21:01:33 +0200

    Initial commit

---
+++ src/client/java/me/thukunua/thukuna/client/ThukunaClient.java
@@ -0,0 +1,10 @@
+package me.thukunua.thukuna.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThukunaClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/thukunua/thukuna/Thukuna.java
@@ -0,0 +1,10 @@
+package me.thukunua.thukuna;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Thukuna implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
