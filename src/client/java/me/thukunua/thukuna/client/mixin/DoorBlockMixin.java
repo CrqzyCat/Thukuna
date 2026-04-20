aaadfb3c9f97f73a11b0743bfd1e21bb6e5370c0
CrqzyCat <powerjak71@gmail.com>
2026-04-20 12:48:23 +0200

    Refactor DoorBlockMixin to play sound on door interaction using interactBlock method

--- src/client/java/me/thukunua/thukuna/client/mixin/DoorBlockMixin.java
+++ src/client/java/me/thukunua/thukuna/client/mixin/DoorBlockMixin.java
@@ -2,18 +2,19 @@ package me.thukunua.thukuna.client.mixin;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.DoorBlock;
-import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.client.sound.AbstractSoundInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,25 +23,26 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
-@Mixin(DoorBlock.class)
+@Mixin(ClientPlayerInteractionManager.class)
 public class DoorBlockMixin {
 
     private static final Identifier HELLO_GOJO_ID = Identifier.of("thukuna", "hello_gojo");
 
-    @Inject(method = "onUse", at = @At("HEAD"))
-    private void onDoorOpen(BlockState state, World world, net.minecraft.util.math.BlockPos pos,
-                            PlayerEntity player, BlockHitResult hit,
-                            CallbackInfoReturnable<ActionResult> cir) {
-        // Nur clientseitig und nur beim unteren Teil der Tür (sonst wird es 2x gefeuert)
-        if (!(world instanceof net.minecraft.client.world.ClientWorld)) return;
-        if (state.get(Properties.DOUBLE_BLOCK_HALF) != DoubleBlockHalf.LOWER) return;
+    @Inject(method = "interactBlock", at = @At("HEAD"))
+    private void onInteractBlock(PlayerEntity player, World world, Hand hand,
+                                  BlockHitResult hitResult,
+                                  CallbackInfoReturnable<ActionResult> cir) {
+        BlockPos pos = hitResult.getBlockPos();
+        BlockState state = world.getBlockState(pos);
 
-        SoundEvent event = SoundEvent.of(HELLO_GOJO_ID);
-        AbstractSoundInstance instance = new AbstractSoundInstance(event, SoundCategory.MASTER, Random.create()) {
-            @Override public float getVolume() { return 1.0f; }
-            @Override public float getPitch()  { return 1.0f; }
-            @Override public boolean isRepeatable() { return false; }
-        };
-        MinecraftClient.getInstance().getSoundManager().play(instance);
+        if (state.getBlock() instanceof DoorBlock) {
+            SoundEvent event = SoundEvent.of(HELLO_GOJO_ID);
+            AbstractSoundInstance instance = new AbstractSoundInstance(event, SoundCategory.MASTER, Random.create()) {
+                @Override public float getVolume() { return 1.0f; }
+                @Override public float getPitch()  { return 1.0f; }
+                @Override public boolean isRepeatable() { return false; }
+            };
+            MinecraftClient.getInstance().getSoundManager().play(instance);
+        }
     }
 }
