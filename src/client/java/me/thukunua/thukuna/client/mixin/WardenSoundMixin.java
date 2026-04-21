e776aa0d2999daf9867ac0727941350a69ac4323
CrqzyCat <powerjak71@gmail.com>
2026-04-21 12:19:55 +0200

    Refactor WardenSoundMixin to replace Sonic Boom sound with custom sound and update play sound method

--- src/client/java/me/thukunua/thukuna/client/mixin/WardenSoundMixin.java
+++ src/client/java/me/thukunua/thukuna/client/mixin/WardenSoundMixin.java
@@ -4,36 +4,30 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.sound.AbstractSoundInstance;
-import net.minecraft.entity.mob.WardenEntity;
+import net.minecraft.client.sound.SoundManager;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
-@Mixin(WardenEntity.class)
+@Mixin(SoundManager.class)
 public class WardenSoundMixin {
 
     private static final Identifier HOLLOW_PURPLE_ID = Identifier.of("thukuna", "hollow_purple");
 
-    @Inject(
-        method = "playSound(Lnet/minecraft/sound/SoundEvent;FF)V",
-        at = @At("HEAD"),
-        cancellable = true
-    )
-    private void onPlaySound(SoundEvent sound, float volume, float pitch, CallbackInfo ci) {
-        // Nur clientseitig
-        if (!((WardenEntity)(Object)this).getWorld().isClient) return;
+    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
+    private void onPlay(net.minecraft.client.sound.SoundInstance sound, CallbackInfo ci) {
+        if (sound.getId() != null &&
+            sound.getId().equals(SoundEvents.ENTITY_WARDEN_SONIC_BOOM.value().getId())) {
 
-        // Nur den Sonic Boom Sound ersetzen
-        if (sound == SoundEvents.ENTITY_WARDEN_SONIC_BOOM) {
-            ci.cancel(); // Originalen Sound unterdrücken
+            ci.cancel();
 
             SoundEvent hollowPurple = SoundEvent.of(HOLLOW_PURPLE_ID);
             AbstractSoundInstance instance = new AbstractSoundInstance(hollowPurple, SoundCategory.HOSTILE, Random.create()) {
@@ -41,7 +35,10 @@ public class WardenSoundMixin {
                 @Override public float getPitch()  { return 1.0f; }
                 @Override public boolean isRepeatable() { return false; }
             };
-            MinecraftClient.getInstance().getSoundManager().play(instance);
+            // Direkt auf der SoundManager-Instanz aufrufen ohne erneut zu triggern
+            MinecraftClient.getInstance().execute(() ->
+                MinecraftClient.getInstance().getSoundManager().play(instance)
+            );
         }
     }
 }
