1f524ba49818e60b416a2cb163d06b5a6238e299
CrqzyCat <powerjak71@gmail.com>
2026-04-21 12:24:56 +0200

    Refactor WardenSoundMixin to replace Sonic Boom sound with a custom sound and update playback logic

--- src/client/java/me/thukunua/thukuna/client/mixin/WardenSoundMixin.java
+++ src/client/java/me/thukunua/thukuna/client/mixin/WardenSoundMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.client.sound.SoundManager;
 import net.minecraft.client.sound.SoundSystem;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,7 +20,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(SoundManager.class)
 public class WardenSoundMixin {
 
-    private static final Identifier HOLLOW_PURPLE_ID = Identifier.of("thukuna", "hollow_purple");
+    private static final Identifier WARDEN_SONIC_BOOM = Identifier.of("minecraft", "entity.warden.sonic_boom");
+    private static final Identifier HOLLOW_PURPLE_ID  = Identifier.of("thukuna", "hollow_purple");
 
     @Inject(
         method = "play(Lnet/minecraft/client/sound/SoundInstance;)Lnet/minecraft/client/sound/SoundSystem$PlayResult;",
@@ -29,10 +29,8 @@ public class WardenSoundMixin {
         cancellable = true
     )
     private void onPlay(SoundInstance sound, CallbackInfoReturnable<SoundSystem.PlayResult> cir) {
-        if (sound.getId() != null &&
-            sound.getId().equals(SoundEvents.ENTITY_WARDEN_SONIC_BOOM.getId())) {
-
-            cir.setReturnValue(SoundSystem.PlayResult.REPLACED_BY_SILENCE);
+        if (WARDEN_SONIC_BOOM.equals(sound.getId())) {
+            cir.setReturnValue(SoundSystem.PlayResult.NOT_STARTED);
 
             SoundEvent hollowPurple = SoundEvent.of(HOLLOW_PURPLE_ID);
             AbstractSoundInstance instance = new AbstractSoundInstance(hollowPurple, SoundCategory.HOSTILE, Random.create()) {
